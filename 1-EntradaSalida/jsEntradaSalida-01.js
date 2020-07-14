f4a913966caf27f22ad421548ab7bb0f7ef5b3cb
gomezfernando1995 <gomezfernando1995@gmail.com>
2020-07-13 22:52:51 -0300

    Cambio primera clase 13/7

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,9 @@
 /*
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
+
 function mostrar()
 {
-	alert("hola estas anto ");
+	alert("Esto funciona de maravilla");
 }
 
--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,6 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	let NombreSelecto = prompt("Dame tu nombre"); //Aca se declara el nombre que se elija y se carga en la variable
+    alert("Tu te llamas " + NombreSelecto );     // mensaje con la variable elegida
 }
-
